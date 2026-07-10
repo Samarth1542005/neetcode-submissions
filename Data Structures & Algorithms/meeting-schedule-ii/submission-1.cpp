989d8d5388c8e7cf539d6afbc2645f5913001a95
Samarth Jagdish Raut <157493139+Samarth1542005@users.noreply.github.com>
2026-07-10 14:10:22 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int n = intervals.size();
+        if(n == 0) return 0;
+
+        vector<int> start, end;
+
+        for(auto &it : intervals)
+        {
+            start.push_back(it.start);
+            end.push_back(it.end);
+        }
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int rooms = 0;
+        int ans = 0;
+
+        int i = 0, j = 0;
+
+        while(i < n)
+        {
+            if(start[i] < end[j])
+            {
+                rooms++;
+                ans = max(ans, rooms);
+                i++;
+            }
+            else
+            {
+                rooms--;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
